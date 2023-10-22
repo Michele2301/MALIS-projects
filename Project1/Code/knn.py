1d0933a20b2d757e32d6313cf7843ea55e16a02f
Michele2301 <micheleferrero9@gmail.com>
2023-10-22 15:57:28 +0200

    adding some steps to knn

--- Project1/Code/knn.py
+++ Project1/Code/knn.py
@@ -1,6 +1,7 @@
 from scipy.spatial import distance_matrix
 import numpy as np
 
+
 class KNN:
     '''
     k nearest neighboors algorithm class
@@ -17,33 +18,39 @@ class KNN:
 
         if k <= 0:
             raise Exception("Sorry, no numbers below or equal to zero. Start again!")
-            
+
         # empty initialization of X and y
         self.X = []
         self.y = []
         # k is the parameter of the algorithm representing the number of neighborhoods
         self.k = k
-        
-    def train(self,X,y):
+
+    def train(self, X, y):
         '''
         INPUT :
         - X : is a 2D NxD numpy array containing the coordinates of points
         - y : is a 1D Nx1 numpy array containing the labels for the corrisponding row of X
-        '''        
-       
-    def predict(self,X_new,p):
+        '''
+        self.X = X
+        self.y = y
+
+    def predict(self, X_new, p):
         '''
         INPUT :
         - X_new : is a MxD numpy array containing the coordinates of new points whose label has to be predicted
         A
         OUTPUT :
         - y_hat : is a Mx1 numpy array containing the predicted labels for the X_new points
-        ''' 
-            
-
+        '''
+        ''' finds the indexes of the biggest k numbers'''
+        indexes = np.argpartition(self.minkowski_dist(X_new, p), -self.k)[:, -self.k:]
+        ''' map them to values, so now we have arrays of the nearest labels '''
+        values = [[self.y[col] for col in col_indexes] for col_indexes in indexes]
+        ''' still need to reduce to the most common'''
+        ''' TODO '''
         return y_hat
-    
-    def minkowski_dist(self,X_new,p):
+
+    def minkowski_dist(self, X_new, p):
         '''
         INPUT : 
         - X_new : is a MxD numpy array containing the coordinates of points for which the distance to the training set X will be estimated
@@ -52,5 +59,8 @@ class KNN:
         OUTPUT :
         - dst : is an MxN numpy array containing the distance of each point in X_new to X
         '''
-        
+        dst = []
+        for i in range(0, X_new.size):
+            for j in range(0, self.X.shape[0]):
+                dst[i][j] = np.power(np.sum(np.power(np.abs(X_new[i] - self.X[j]), p)), 1 / p)
         return dst
