4a99250636765412a7902b20b6b0e5b9d63e6865
Michele2301 <micheleferrero9@gmail.com>
2023-11-20 22:29:31 +0100

    added project2

---
+++ Project2/Code/perceptron.py
@@ -0,0 +1,15 @@
+"""
+Project 2 - Perceptron
+F. Dente, M. Ferrero
+"""
+
+
+class Perceptron:
+    def __int__(self):
+        weights = []
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
