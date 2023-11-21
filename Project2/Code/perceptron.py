40955d31b45f3125176040757f685c970f34b08d
Michele2301 <micheleferrero9@gmail.com>
2023-11-21 12:09:43 +0100

    changed some stuff, now the validation is made on the validation set

--- Project2/Code/perceptron.py
+++ Project2/Code/perceptron.py
@@ -9,18 +9,25 @@ from sklearn.preprocessing import PolynomialFeatures
 
 class Perceptron:
     def __init__(self):
-        self.weights = []
+        self.weights = np.empty(0)
 
-    def train(self, X, y, tries=10):
-        # set the starting alpha
-        alpha = 0.00001
+    def train(self, X, y, Xval, yval, tries=10,alpha=0.001):
+        """
+        INPUT :
+        - X : is a 2D NxD numpy array containing the coordinates of points
+        - y : is a 1D Nx1 numpy array containing the labels for the corrisponding row of X
+        - tries : is a natural number representing the number of tries to find the best perceptron hyperplane
+        """
+        # setting weights to 0
+        self.weights = np.zeros(X.shape[1] + 1)
         # add the offset b
         X = PolynomialFeatures(1).fit_transform(X)
-        # generate random vector between 0 and 1
+        Xval = PolynomialFeatures(1).fit_transform(Xval)
+        # set the minimum to the loss function of some starting weights
+        weights = np.random.rand(X.shape[1])
+        min = loss_function(weights, Xval, yval)
         for i in range(tries):
-            weights = np.random.rand(X.shape[1])
-            self.weights = weights
-            min = (self.weights.reshape(1, self.weights.shape[0]).dot(X.transpose())).sum()
+            # generate random vector between 0 and 1 for each try
             while True:
                 # we start with a random vector of weights, N+1 because of the offset b
                 m = 0
@@ -30,14 +37,29 @@ class Perceptron:
                         m += 1
                 if m == 0:
                     break
-            if (weights.reshape(1,self.weights.shape[0]).dot(X.transpose())).sum()<min:
-                min=(self.weights.reshape(1,self.weights.shape[0]).dot(X.transpose())).sum()
+            # given what she said on the paper we should use the validation to find the best weights
+            if loss_function(weights, Xval, yval) < min:
+                min = loss_function(weights, Xval, yval)
                 self.weights = weights
-            print((weights.reshape(1,self.weights.shape[0]).dot(X.transpose())).sum())
+                # print("min: ", min)
+            # change starting point
+            # print("loss is: ", loss_function(weights, Xval, yval))
+            weights = np.random.rand(X.shape[1])
 
-    def predict(self, X_new):
-        print(self.weights.shape)
-        return np.sign(self.weights[1:].reshape(1,self.weights[1:].shape[0]).dot(X_new.transpose()) + self.weights[0])
+    def predict(self, X):
+        return np.sign(self.weights[1:].transpose().dot(X.transpose()) + self.weights[0])
+
+
+def loss_function(weights, X, y):
+    """
+    INPUT :
+    - weights : is a 1D numpy array containing the weights of the model (D+1 elements)
+    - X : is a 2D Nx(D+1) numpy array containing the coordinates of points
+    - y : is a 1D Nx1 numpy array containing the labels for the corrisponding row of X
+    """
+    value = -1 * ((weights.transpose() / abs(weights)).dot(X.transpose()) * y).sum()
+    return value
+
 
 if __name__ == "__main__":
     import pandas as pd
@@ -50,9 +72,9 @@ if __name__ == "__main__":
     data0 = data[data['target'] == 0]
     data1 = data[data['target'] == 1]
     data = pd.concat([data0, data1])
-    Xdata, Xtest, ydata, ytest = train_test_split(data.values[:, 0:-1], data.values[:, -1]*2-1, test_size=0.2,
-                                                  random_state=0, stratify=data.values[:, -1]*2-1)
+    Xdata, Xtest, ydata, ytest = train_test_split(data.values[:, 0:-1], data.values[:, -1] * 2 - 1, test_size=0.2,
+                                                  random_state=0, stratify=data.values[:, -1] * 2 - 1)
     Xtrain, Xval, ytrain, yval = train_test_split(Xdata, ydata, test_size=0.2, random_state=0, stratify=ydata)
     perceptron = Perceptron()
-    perceptron.train(Xtrain, ytrain)
+    perceptron.train(Xtrain, ytrain, Xval, yval, tries=5000, alpha=0.0001)
     print(np.mean(perceptron.predict(Xval) == yval))
