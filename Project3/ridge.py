ba844f9a7da36bda36dbc3d51e5e37fe21ded0db
Francesco Dente s313355 <s313355@studenti.polito.it>
2024-01-21 19:13:28 +0100

    deliverable(Project 3): first draft

--- Project3/ridge.py
+++ Project3/ridge.py
@@ -10,10 +10,22 @@ def transform(X):
 
 class Ridge_Regression:
     def __init__(self, lambda_value=0):
+        """
+        Ridge regression model.
+        
+        Args:
+            lambda_value = 0: regularization coefficient
+        """
         self.lambda_value = lambda_value
         self.weights = None
 
     def Train(self, X, y):
+        """Train the ridge regression model using the closed form solution. The bias term is not regularized.
+        
+        Args:
+            X: numpy array of dimension (N x M), where [N] is the number of training samples and [M] is the number of features
+            y: numpy array of dimension (N x 1), where [N] is the number of training samples
+        """
         X = transform(X)
         I = np.eye(X.shape[1])
         I[0,0] = 0
@@ -21,6 +33,14 @@ class Ridge_Regression:
         return
 
     def Predict(self, x):
+        """
+        Make a prediction on test samples x.
+        
+        Args:
+            x: numpy array of test samples of dimension (N x M), where [N] is the number of test samples and [M] is the number of features
+        Returns:
+            numpy array of predictions of dimension (N x 1), where [N] is the number of test samples
+        """
         return transform(x).dot(self.weights)
 
     def __str__(self):
